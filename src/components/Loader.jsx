9a3d48ef77ae8fb47b8c0b1af0f2293246cadc60
Abhishek <Abhishekdevpanwar@gmail.com>
2022-07-17 18:25:13 +0530

    Aaa

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Hypnosis } from "react-cssfx-loading";
 const Loader = () => {
   return (
-    <div className="flex w-full h-screen justify-center items-center">
+    <div className="flex w-full h-screen justify-center items-center bg-slate-100">
       <Hypnosis color="#333" width="100px" height="100px  " duration="3s" />
     </div>
   );
